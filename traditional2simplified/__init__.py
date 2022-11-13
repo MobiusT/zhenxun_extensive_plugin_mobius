e7d2ca4f271fe652992dfce0b68774be7da13685
MobiusT <13402190140@qq.com>
2022-11-13 17:16:17 +0800

    修改翻译平台，使用百度翻译

--- traditional2simplified/__init__.py
+++ traditional2simplified/__init__.py
@@ -3,7 +3,7 @@ from nonebot.adapters.onebot.v11 import MessageEvent, Message
 from nonebot.params import CommandArg
 from services.log import logger
 from configs.config import Config
-import requests, json
+import requests, json, time, hashlib, httpx
 
 __zx_plugin_name__ = "繁中转简中"
 __plugin_usage__ = """
@@ -23,19 +23,15 @@ __plugin_settings__ = {
     "limit_superuser": False,
     "cmd": ["簡體", "簡體字", "簡中"],
 }
-__plugin_cd_limit__ = {
-    "cd": 3,
-    "rst": "[at]慢点说！"
-}
 __plugin_configs__ = {
     "APPID": {
         "value": '', 
-        "help": "https://www.mxnzp.com/获取app_id", 
+        "help": "http://api.fanyi.baidu.com/获取app_id", 
         "default_value": ''
     },
     "APPSECRET": {
         "value": '',
-        "help": "https://www.mxnzp.com/获取app_secret", 
+        "help": "http://api.fanyi.baidu.com/获取app_secret", 
         "default_value": ''
     },
 }
@@ -50,22 +46,40 @@ async def _(event: MessageEvent, arg: Message = CommandArg()):
     app_id = Config.get_config("traditional2simplified", "APPID")
     app_secret = Config.get_config("traditional2simplified", "APPSECRET")
     if not app_id:
-        msg='未配置app_id, 请前往https://www.mxnzp.com/获取'
+        msg='未配置app_id, 请前往http://api.fanyi.baidu.com/获取'
         logger.error(msg)
         await convert.finish(msg)
     elif not app_secret:
-        msg='未配置app_secret, 请前往https://www.mxnzp.com/获取'
+        msg='未配置app_secret, 请前往http://api.fanyi.baidu.com/获取'
         logger.error(msg)
         await convert.finish(msg)
     try:
         #调用api
-        rs = requests.get(f'https://www.mxnzp.com/api/convert/zh?content={text}&type=2&app_id={app_id}&app_secret={app_secret}')
-        rs_obj = json.loads(rs.text)
-        msg=rs_obj["data"]["convertContent"]
+        msg = await translate(text, app_id, app_secret, "cht", "zh")      
         await convert.send(msg)
     except Exception as e:
         msg=f'转换出错{type(e)}：{e}'
         logger.error(msg)
         await convert.send(msg)
-   
+
+async def translate(text: str, appid: str, apikey: str, lang_from: str = "auto", lang_to: str = "zh") -> str:
+    salt = str(round(time.time() * 1000))
+    sign_raw = appid + text + salt + apikey
+    sign = hashlib.md5(sign_raw.encode("utf8")).hexdigest()
+    params = {
+        "q": text,
+        "from": lang_from,
+        "to": lang_to,
+        "appid": appid,
+        "salt": salt,
+        "sign": sign,
+    }
+    url = "https://fanyi-api.baidu.com/api/trans/vip/translate"
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, params=params)
+        result = resp.json()
+    try:
+        return result["trans_result"][0]["dst"]
+    except Exception:
+        return str(result)
 
