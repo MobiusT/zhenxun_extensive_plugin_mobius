6a2cc8e1b39863d00de2cdb2301b028b0dd15aad
MobiusT <13402190140@qq.com>
2022-11-14 20:22:05 +0800

    修复bug，增加列表查看

--- resendRecallMsgByqid/__init__.py
+++ resendRecallMsgByqid/__init__.py
@@ -14,7 +14,7 @@ __plugin_usage__ = """
 usage：
     禁止发烧[at]/[qq]  加入禁止发烧列表
     允许发烧[at]/[qq]  移除禁止发烧列表
-
+    禁止发烧列表
     群内指定qq撤回消息时自动将撤回的消息发送至群内
 """.strip()
 __plugin_des__ = "群内指定qq撤回消息时自动将撤回的消息发送至群内"
@@ -27,8 +27,9 @@ __plugin_version__ = 0.1
 __plugin_author__ = "mobius"
 
 groupRecall = on_notice(priority=1, block=False)
-listenList = on_command("禁止发烧", priority=5, permission=SUPERUSER, block=True)
+addListenList = on_command("禁止发烧", priority=5, permission=SUPERUSER, block=True)
 unListenList = on_command("允许发烧", priority=5, permission=SUPERUSER, block=True)
+listenList = on_command("禁止发烧列表", priority=6, permission=SUPERUSER, block=True)
 jsonFile = Path(os.path.dirname(os.path.abspath(__file__))) / "recall.json"
 
 # 检测撤回消息
@@ -41,7 +42,7 @@ async def if_withdraw_handle(bot: Bot, event: GroupRecallNoticeEvent):
         recall_message_content = await bot.get_msg(message_id=recall_message_id)
         print(recall_message_content["message"])
         #q群
-        gid=event.group_id
+        gid=str(event.group_id)
         #撤回者qq
         qid=event.user_id
         data: dict = load_data()    
@@ -65,11 +66,11 @@ async def if_withdraw_handle(bot: Bot, event: GroupRecallNoticeEvent):
                 #发送消息
                 await bot.send_group_forward_msg(group_id=gid, messages=mes_list)
         except:
-            logger.info("无禁止发烧名单")               
+            logger.info("无禁止发烧成员")               
        
 
 
-@listenList.handle()
+@addListenList.handle()
 async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     #获取参数
     qid = arg.extract_plain_text().strip()
@@ -77,10 +78,9 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     if qid:
         try:
             qidUser = await bot.get_group_member_info(group_id=event.group_id, user_id=qid)
-
             qidList.append(qid)
         except Exception:
-            await listenList.send(f"{qid}不是可用的qq号")
+            await addListenList.send(f"{qid}不是可用的qq号")
     #获取at
     ats =  get_message_at(event.json())
     if not len(ats)==0 :
@@ -96,14 +96,14 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
         dataList=[]
     for q in qidList:
         if q in dataList:
-            await listenList.send(f"{q}已经在禁止发烧列表中")
+            await addListenList.send(f"{q}已经在禁止发烧列表中")
         else:
             dataList.append(q)
             msg +=f"{q},"
     data.update({gid: dataList})
     save_data(data)
     if msg:
-        await listenList.send(msg[:-1]+"已添加")
+        await addListenList.send(msg[:-1]+"已添加")
 
 @unListenList.handle()
 async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
@@ -113,10 +113,9 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     if qid:
         try:
             qidUser = await bot.get_group_member_info(group_id=event.group_id, user_id=qid)
-            print(str(qidUser))
             qidList.append(qid)
         except Exception:
-            await listenList.send(f"{qid}不是可用的qq号")
+            await unListenList.send(f"{qid}不是可用的qq号")
     #获取at
     ats =  get_message_at(event.json())
     if not len(ats)==0 :
@@ -129,17 +128,30 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     try:
         dataList: list = data[gid]            
     except:
-        await listenList.finish(f"{gid}中无禁止发烧成员")
+        await unListenList.finish(f"{gid}中无禁止发烧成员")
     for q in qidList:
         if q in dataList:
             dataList.remove(q)
             msg +=f"{q},"
         else:
-            await listenList.send(f"{q}不在禁止发烧列表中")           
+            await unListenList.send(f"{q}不在禁止发烧列表中")           
     data.update({gid: dataList})
     save_data(data)
     if msg:
-        await listenList.send(msg[:-1]+"已删除")
+        await unListenList.send(msg[:-1]+"已删除")
+
+@listenList.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    gid=str(event.group_id)
+    data: dict = load_data()    
+    try:
+        dataList = data[gid]
+    except Exception:
+        await listenList.finish("本群无禁止发烧人员")  
+    if len(dataList)==0:
+        await listenList.finish("本群无禁止发烧人员")  
+    await listenList.finish(str(dataList))  
+
 
 #反序列化文件
 def load_data():
