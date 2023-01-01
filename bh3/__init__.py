ecf801a027a6c3a09352137576b10aa44f8fc8a5
MobiusT <13402190140@qq.com>
2023-01-01 16:16:29 +0800

    关闭无用sql日志（尝试性）

--- bh3/__init__.py
+++ bh3/__init__.py
@@ -1,4 +1,6 @@
-import nonebot
+import nonebot, logging
 
+logging.basicConfig()
+logging.getLogger("sqlitedict.SqliteMultithread").setLevel(logging.ERROR) 
 nonebot.load_plugins("zhenxun_extensive_plugin_mobius/bh3")
 
