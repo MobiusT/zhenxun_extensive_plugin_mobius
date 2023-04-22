8bd6adea06343ed1c275339f8852892690d8a825
MobiusT <62053271+MobiusT@users.noreply.github.com>
2023-04-22 13:21:13 +0800

    规范代码

--- bh3/__init__.py
+++ bh3/__init__.py
@@ -1,11 +1,7 @@
-'''
-Author: MobiusT
-Date: 2023-01-14 19:08:36
-LastEditors: MobiusT
-LastEditTime: 2023-01-14 19:10:52
-'''
-import nonebot, logging
 from pathlib import Path
+
+import logging
+import nonebot
 
 logging.basicConfig()
 logging.getLogger("sqlitedict.SqliteMultithread").setLevel(logging.ERROR)
