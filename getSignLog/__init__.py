9f2256028d11c9d1ea73fa8f7f4f825d22272802
MobiusT <62053271+MobiusT@users.noreply.github.com>
2023-04-27 20:52:03 +0800

    补充群推送，避免风控

--- getSignLog/__init__.py
+++ getSignLog/__init__.py
@@ -1,12 +1,13 @@
-from utils.utils import get_bot, scheduler
-from nonebot import on_command
-from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageEvent, MessageSegment
-from services.log import logger
-from utils.manager import group_manager
-from configs.config import Config
-from nonebot_plugin_htmlrender import text_to_pic
 import os
 
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageEvent, MessageSegment
+from nonebot_plugin_htmlrender import text_to_pic
+
+from configs.config import Config
+from services.log import logger
+from utils.manager import group_manager
+from utils.utils import get_bot, scheduler
 
 __zx_plugin_name__ = "定时获取签到日志 [Superuser]"
 __plugin_usage__ = """
@@ -28,54 +29,68 @@ __plugin_settings__ = {
 }
 __plugin_configs__ = {
     "PUSHQQ": {
-        "value": '', 
-        "help": "需要推送的qq，多个qq用逗号分隔，超级用户无需填写，会自动推送", 
+        "value": '',
+        "help": "需要推送的qq，多个qq用逗号分隔，超级用户无需填写，会自动推送",
         "default_value": ''
     },
 }
+__plugin_task__ = {"getSignLog": "获取签到日志"}
+
+Config.add_plugin_config(
+    "_task",
+    "DEFAULT_GETSIGNLOG",
+    False,
+    help_="被动 定时获取签到日志 进群默认开关状态",
+    default_value=False,
+)
 
 getSignLog = on_command("查日志", priority=15, block=True)
 
-#日志输出位置，通过getResult.sh输出
-LOG_PATH =  os.path.join(os.path.dirname(__file__), "log/sign.log")
+# 日志输出位置，通过getResult.sh输出
+LOG_PATH = os.path.join(os.path.dirname(__file__), "log/sign.log")
+
 
 @getSignLog.handle()
-async def _(event: MessageEvent,):
-    logFile = open(LOG_PATH,'r')
+async def _(event: MessageEvent, ):
+    log_file = open(LOG_PATH, 'r')
     try:
-        logStr = logFile.read()
-        pic = await text_to_pic(text=logStr)#日志转图片，依赖nonebot_plugin_htmlrender
+        log_str = log_file.read()
+        pic = await text_to_pic(text=log_str)  # 日志转图片，依赖nonebot_plugin_htmlrender
         await getSignLog.send(MessageSegment.image(pic))
         logger.info(
             f"(USER {event.user_id}, GROUP {event.group_id if isinstance(event, GroupMessageEvent) else 'private'})"
             f" 发送签到日志"
         )
     finally:
-        logFile.close()
+        log_file.close()
 
 
-@scheduler.scheduled_job(#定时任务，每日6时6分
+@scheduler.scheduled_job(  # 定时任务，每日6时6分
     "cron",
     hour=6,
     minute=6,
 )
 async def _():
-    # 每日查看
-    bot = get_bot()
-    if bot:
-        logFile = open(LOG_PATH,'r')
-        try:
-            logStr = logFile.read()
-            pic = await text_to_pic(text=logStr)#日志转图片，依赖nonebot_plugin_htmlrender
-            qidList:list = (Config.get_config("getSignLog", "PUSHQQ")).split(',')
-            for superuser in bot.config.superusers:
-                qidList.append(superuser)
-            qidList=list(set(qidList))
-            for qid in qidList:
-                try:
-                    await bot.send_private_msg(user_id=int(qid), message=MessageSegment.image(pic))  
-                except:
-                    logger.error(f"{qid}签到日志推送出错")          
-        finally:
-            logFile.close()
-
+    if bot := get_bot():
+        return
+    gl = await bot.get_group_list()
+    gl = [g["group_id"] for g in gl]
+    log_file = open(LOG_PATH, 'r')
+    try:
+        # 推送群
+        log_str = log_file.read()
+        pic = await text_to_pic(text=log_str)  # 日志转图片，依赖nonebot_plugin_htmlrender
+        for gid in gl:
+            if group_manager.check_group_task_status(gid, "getSignLog"):
+                await bot.send_group_msg(group_id=int(gid), message=MessageSegment.image(pic))
+        # 推送qq列表
+        qid_list: list = (Config.get_config("getSignLog", "PUSHQQ")).split(',')
+        qid_list.extend(iter(bot.config.superusers))
+        qid_list = list(set(qid_list))
+        for qid in qid_list:
+            try:
+                await bot.send_private_msg(user_id=int(qid), message=MessageSegment.image(pic))
+            except:
+                logger.error(f"{qid}签到日志推送出错")
+    finally:
+        log_file.close()
